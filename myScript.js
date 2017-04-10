6f67d66a19837b532d8f4d6ed5ebe4e6084037b5
Jesús <Jess>
2017-04-10 16:34:34 -0400

    mariqueando

--- myScript.js
+++ myScript.js
@@ -1,6 +1,8 @@
 var indice = 0;
 var colores = ["blue", "yellow", "red", "green", "cyan", "pink"];
 
+var indi = colores.length;
+
 function cambiarColor(){
   if(indice == colores.length){
     indice = 0;
@@ -8,6 +10,15 @@ function cambiarColor(){
 
   document.getElementById("titulo").style.color = colores[indice];
   indice ++;
+}
+
+function cambiarFondo(){
+  if(indi == -1){
+    indi = colores.length;
+  }
+
+  document.getElementById("division_02").style.background-color = colores[indi];
+  indi --;
 }
 
 var intervalo = setInterval(cambiarColor, 80);
