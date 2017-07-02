64dac67e9fbdaa3bb9905fc85e00e96889c5ef66
Jesús <Jess>
2017-07-01 23:53:59 -0400

    arreglando pestaña de thg

--- js/script_thoughts.js
+++ js/script_thoughts.js
@@ -3,3 +3,29 @@ $(function(){
     "background-image": "url('imagenes/fondos/fondo_thoughts.jpg')"
   });
 });
+
+var pen_act = 12
+
+function verPensamiento(n){
+
+  var pensamientos = document.getElementsByClassName("pensamiento");
+
+  pen_act += n;
+
+  if(pen_act > pensamientos.length){
+    pen_act = 1;
+  }
+
+  if(pen_act == 0){
+    pen_act = pensamientos.length - 1;
+  }
+
+  for(var i = 0; i < pensamientos.length; i++){
+    pensamientos[i].style.display = "none";
+  }
+
+  pensamientos[pen_act - 1].style.display = "block";
+
+  console.log(pen_act);
+
+}
